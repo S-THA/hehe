003d9b72baa1e3e5219721757aae89b32edb2bad
S-THA <95969180+S-THA@users.noreply.github.com>
2022-02-16 12:22:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     
-    messDesc: 'Mùng năm 8h anh qua đón nhé công chúa.',
+    messDesc: 'bấm vô này nè.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/sitha.knt1608' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
